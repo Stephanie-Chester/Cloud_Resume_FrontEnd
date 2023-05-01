905f37ef05009b768c1f7d611d9fd5457fef4812
Stephanie Chester <schester81@gmail.com>
2023-05-01 13:45:37 -0700

    added visitor api cypress test

---
+++ cypress/e2e/visitorAPI.cy.js
@@ -0,0 +1,13 @@
+describe('visitorCount api', () => {
+    context('POST /IncrementVisitorCount', () => {
+        it('should return a visitor count', () => {
+            cy.request({
+                method: 'POST',
+                url: 'https://7hn5blxzxg.execute-api.us-east-1.amazonaws.com/default/IncrementVisitorCounter'
+            })
+                .should((response) => {
+                    cy.log(JSON.stringify(response.body))
+                });
+        });
+    });
+});
