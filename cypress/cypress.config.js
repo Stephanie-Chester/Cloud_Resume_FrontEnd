edaf3d0f81fa7732a1bc27d713f64b4fb9cd0d2c
Stephanie Chester <schester81@gmail.com>
2023-05-01 10:51:06 -0700

    cypress config modified

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     projectId: "r6v848",
     fixturesFolder: false,
+    integrationFolder: "cypress/e2e",
   e2e: {
     baseUrl: 'https://www.stephaniechester.live/',
     setupNodeEvents(on, config) {},
