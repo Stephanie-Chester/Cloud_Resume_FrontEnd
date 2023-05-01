fd0ec443e4a5bb8e4c816943cdd8c29fb08ae03e
Stephanie Chester <schester81@gmail.com>
2023-05-01 09:04:18 -0700

    specpattern added

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -6,5 +6,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
     supportFile: false,
+    specPattern: './cypress/e2e/*.cy.{js,jsx,ts,tsx}'
 },
 })   // The rest of the Cypress config options go here...
